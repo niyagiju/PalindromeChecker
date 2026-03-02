d356c849bbfd8996b8e47d58b68e5b15a9acabfd
niyagiju <gijuniya@gmail.com>
2026-03-02 08:42:50 +0530

    UC11 CREATED

--- PalindromeCheckerApp/src/PalindromeChecker.java
+++ PalindromeCheckerApp/src/PalindromeChecker.java
@@ -1,41 +1,26 @@
+class PalindromeChecker {
 
-import java.util.Scanner;
+    // Public method exposed to outside
+    public boolean checkPalindrome(String input) {
 
-public class CaseInsensitivePalindrome {
+        if (input == null)
+            return false;
 
-    public static boolean isPalindrome(String str) {
+        // Preprocessing (optional improvement)
+        String normalized = input.toLowerCase().replaceAll("\\s+", "");
+
+        char[] chars = normalized.toCharArray();
         int start = 0;
-        int end = str.length() - 1;
+        int end = chars.length - 1;
 
         while (start < end) {
-            if (str.charAt(start) != str.charAt(end))
+            if (chars[start] != chars[end])
                 return false;
+
             start++;
             end--;
         }
+
         return true;
-    }
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("UC10: Case-Insensitive & Space-Ignored Palindrome");
-        System.out.print("Input: ");
-
-        String input = sc.nextLine();
-
-        // Normalize string
-        String normalized = input
-                .toLowerCase()                // ignore case
-                .replaceAll("\\s+", "");      // remove spaces
-
-        boolean result = isPalindrome(normalized);
-
-        if (result)
-            System.out.println("It is a Palindrome.");
-        else
-            System.out.println("It is NOT a Palindrome.");
-
-        sc.close();
     }
 }
