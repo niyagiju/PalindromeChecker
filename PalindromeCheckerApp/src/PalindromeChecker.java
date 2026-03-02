345cff40e4851646576259cc5daf72c55df117b9
niyagiju <gijuniya@gmail.com>
2026-03-02 09:09:46 +0530

    main method added
    Added a main method to PalindromeChecker for user input.

--- PalindromeCheckerApp/src/PalindromeChecker.java
+++ PalindromeCheckerApp/src/PalindromeChecker.java
@@ -1,18 +1,13 @@
-class PalindromeChecker {
-
-    // Public method exposed to outside
+import java.util.Scanner;
+public class PalindromeChecker {
     public boolean checkPalindrome(String input) {
-
         if (input == null)
             return false;
-
-        // Preprocessing (optional improvement)
         String normalized = input.toLowerCase().replaceAll("\\s+", "");
 
         char[] chars = normalized.toCharArray();
         int start = 0;
         int end = chars.length - 1;
-
         while (start < end) {
             if (chars[start] != chars[end])
                 return false;
@@ -20,7 +15,20 @@ class PalindromeChecker {
             start++;
             end--;
         }
-
         return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("UC11: Object-Oriented Palindrome Service");
+        System.out.print("Input: ");
+        String input = sc.nextLine();
+        PalindromeChecker checker = new PalindromeChecker();
+        boolean result = checker.checkPalindrome(input);
+        if (result)
+            System.out.println("It is a Palindrome.");
+        else
+            System.out.println("It is NOT a Palindrome.");
+
+        sc.close();
     }
 }
